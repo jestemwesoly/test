b2bc9bdda9ac6f208f8d9977e848af354dffab7c
Raf <r.m.markowski@gmail.com>
2023-10-08 00:21:18 +0200

    fixed js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,29 @@
-// const nav = document.querySelector('.nav')
-// const navBtn = document.querySelector('.burger-btn')
-// const allNavItems = document.querySelectorAll('.nav__item')
-// const navBtnBars = document.querySelector('.burger-btn__bars')
-// const allSections = document.querySelectorAll('.section')
-// const footerYear = document.querySelector('.footer__year')
-
-// const handleNav = () => {
-// 	nav.classList.toggle('nav--active')
-
-// 	navBtnBars.classList.remove('black-bars-color')
-
-// 	allNavItems.forEach(item => {
-// 		item.addEventListener('click', () => {
-// 			nav.classList.remove('nav--active')
-// 		})
-// 	})
-
-// 	handleNavItemsAnimation()
-// }
-
-// const handleNavItemsAnimation = () => {
-// 	let delayTime = 0
-
-// 	allNavItems.forEach(item => {
-// 		item.classList.toggle('nav-items-animation')
-// 		item.style.animationDelay = '.' + delayTime + 's'
-// 		delayTime++
-// 	})
-// }
-
-// const handleObserver = () => {
-// 	const currentSection = window.scrollY
-// 	allSections.forEach(section => {
-// 		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-// 			navBtnBars.classList.add('black-bars-color')
-// 		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection) {
-// 			navBtnBars.classList.remove('black-bars-color')
-// 		}
-// 	})
-// }
-
-// const handleCurrentYear = () => {
-// 	const year = new Date().getFullYear()
-// 	footerYear.innerText = year
-// }
-
-// navBtn.addEventListener('click', handleNav)
-// window.addEventListener('scroll', handleObserver)
-// handleCurrentYear()
-
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
+const allNavLinks = document.querySelectorAll('.nav__link')
 const footerYear = document.querySelector('.footer__year')
+const footer = document.querySelector('.footer')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
+
 	navMobile.classList.toggle('nav-mobile--active')
+
+	if (navMobile.classList.contains('nav-mobile--active')) {
+		footer.style.display = 'none' // Ukrywamy stopkę, gdy menu jest aktywne
+	} else {
+		footer.style.display = 'block' // Pokazujemy stopkę, gdy menu jest nieaktywne
+	}
+
+	allNavLinks.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+			navBtn.classList.remove('is-active')
+			footer.style.display = 'block'
+		})
+	})
+
+	// document.body.classList.toggle('sticky-body')
 }
 
 navBtn.addEventListener('click', handleNav)
