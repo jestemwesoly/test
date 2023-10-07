0167e1ed2c55044616f9e2c2607d79eb6d23b9a5
Raf <r.m.markowski@gmail.com>
2023-10-07 14:31:43 +0200

    js pt 1

--- js/main.js
+++ js/main.js
@@ -48,3 +48,21 @@
 // navBtn.addEventListener('click', handleNav)
 // window.addEventListener('scroll', handleObserver)
 // handleCurrentYear()
+
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+}
+
+navBtn.addEventListener('click', handleNav)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
