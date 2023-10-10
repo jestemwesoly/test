e7de2c27b4f5e16375c5986b5bf0fc21e516d09d
Raf <r.m.markowski@gmail.com>
2023-10-10 13:38:52 +0200

    final version v1

--- js/main.js
+++ js/main.js
@@ -20,10 +20,17 @@ const handleNav = () => {
 			navMobile.classList.remove('nav-mobile--active')
 			navBtn.classList.remove('is-active')
 			footer.style.display = 'block'
+			document.body.classList.remove('sticky-body')
 		})
 	})
 
-	// document.body.classList.toggle('sticky-body')
+	// if (document.body.classList.contains('sticky-body')) {
+	// 	document.body.classList.remove('sticky-body')
+	// } else {
+	// 	document.body.classList.add('sticky-body')
+	// }
+
+	document.body.classList.toggle('sticky-body')
 }
 
 navBtn.addEventListener('click', handleNav)
